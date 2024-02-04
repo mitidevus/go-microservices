16fb00b96ef4613c6e5cb96c050fcc3f723b18c3
mitidevus <mitidevus@gmail.com>
2024-02-04 20:28:45 +0700

    feat: set up routes, handlers, helpers, and a web server in logger service

---
+++ logger-service/cmd/api/routes.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// Specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"}, // X-CSRF-Token is added to avoid CSRF attacks
+		ExposedHeaders:   []string{"Link"},                                                    // Only allow headers that we're ok with being public
+		AllowCredentials: true,                                                                // Allows sending cookies
+		MaxAge:           300,                                                                 // Maximum value not ignored by any of major browsers
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/log", app.WriteLog)
+
+	return mux
+}
